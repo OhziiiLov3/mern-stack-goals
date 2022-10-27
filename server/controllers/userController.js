327280a3723bf95b9bd76884c552a355c9e557a8
ohziiilov3 <klbaskerville0520@gmail.com>
2022-10-27 11:09:55 -0700

    authication route working

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -49,6 +49,21 @@ if(user){
 // @access Public
 
 const loginUser = asyncHandler(async (req, res) => {
+const {email, password} = req.body
+// Check for User Email
+const user = await User.findOne({email})
+
+if(user && (await bcrypt.compare(password, user.password))){
+    res.json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+    });
+}else{
+    res.status(400);
+    throw new Error("Invalid credentials");
+}
+
   res.json({ message: "Login User" });
 });
 

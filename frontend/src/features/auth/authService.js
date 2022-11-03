6025409f3d34648595b8b6a1cd1750549e913dea
ohziiilov3 <klbaskerville0520@gmail.com>
2022-11-02 20:53:16 -0700

    frontend auth working :login, logout, register

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -12,6 +12,17 @@ const register = async (userData) => {
     return response.data
 }
 
+// Login User 
+
+// resgister user
+const login = async (userData) => {
+    const response = await axios.post(API_URL + 'login',userData)
+    if(response.data){
+        localStorage.setItem('user',JSON.stringify(response.data))
+    }
+    return response.data
+}
+
 // logout user 
 const logout = () => {
     localStorage.removeItem('user')
@@ -19,6 +30,7 @@ const logout = () => {
 
 const authService = {
     register,
+    login,
     logout,
 
 }

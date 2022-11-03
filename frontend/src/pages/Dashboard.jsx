faec65e30d89550fd179bd4eab49932b1ee32d35
ohziiilov3 <klbaskerville0520@gmail.com>
2022-11-03 12:55:04 -0700

    create new goals from fronted workin

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {useEffect } from 'react'
 import {useNavigate} from 'react-router-dom'
 import {useSelector} from 'react-redux'
+import Goalform from '../components/Goalform'
 
 
 const Dashboard = () => {
@@ -20,10 +21,11 @@ const Dashboard = () => {
   return (
     <>
     <section className='heading'>
-        Dashboard
+       
       <h1>Welcome {user && user.name} </h1>
+      <p> Goals Dashboard</p>
     </section>
-   
+   <Goalform/>
     </>
   )
 }
